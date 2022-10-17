5f884109e7b7cc79b06115e5be5460b055db2b9c
CLQuantizer <ezioxxk@gmail.com>
2022-10-17 15:15:33 +0100

    init

--- glove/test_glove.py
+++ glove/test_glove.py
@@ -10,6 +10,10 @@ ANOTHER_JOURNAL = {"title": "test2", "date": "test", "body": "test"}
 UPDATE_JOURNAL = {"title": "test", "date": "new", "body": "new"}
 
 
-def test_related():
+def test_related_go():
+    response = client.get('/related/' + 'hong')
+    assert response.json()['words'][0] == 'kong'
+
+def test_related_hong():
     response = client.get('/related/' + MAGIC_WORD)
     assert response.json()['words'][0] == 'going'
